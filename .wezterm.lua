33b36bab4890c16c0a7675f56a683873470c205c
Smith Rosser Tom (ETAS-DAP/XPC-UK) <Tom.SmithRosser@etas.com>
2024-12-18 13:20:42 +0000

    update wezterm config, new font

--- .wezterm.lua
+++ .wezterm.lua
@@ -2,19 +2,19 @@ local wezterm = require("wezterm")
 local config = wezterm.config_builder()
 
 -- Default shell
--- config.default_prog = { "C:\\Program Files\\Git\\bin\\bash.exe" }
+config.default_prog = { "powershell", "-nologo" }
 
 -- Appearance
-config.window_padding = { left = 0, right = 0, top = 0, bottom = 0 }
-config.color_scheme = "Catppuccin Mocha (Gogh)"
+config.window_padding = { left = 8, right = 8, top = 8, bottom = 0 }
+config.color_scheme = "Everforest Dark (Gogh)"
 
 -- Font
 config.font_size = 11
 config.font = wezterm.font_with_fallback({
-	{ family = "Rec Mono Casual", weight = "Medium", harfbuzz_features = { "calt=1", "clig=1", "liga=1" } },
+	{ family = "Rec Mono Casual", weight = 600, harfbuzz_features = { "calt=1", "clig=1", "liga=1" } },
 	{ family = "Symbols Nerd Font", scale = 1 },
 })
-config.line_height = 1.1
+-- config.line_height = 1.2
 
 -- Behaviour
 config.enable_tab_bar = false
