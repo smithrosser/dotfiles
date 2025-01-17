30281e77986b2d50d61d4c26d05e7306848c42be
Smith Rosser Tom (ETAS-DAP/XPC-UK) <Tom.SmithRosser@etas.com>
2025-01-17 12:59:18 +0000

    add platform check in wezterm config

--- .wezterm.lua
+++ .wezterm.lua
@@ -1,21 +1,27 @@
 local wezterm = require("wezterm")
 local config = wezterm.config_builder()
 
+-- Default shell
+if wezterm.target_triple == "x86_64-pc-windows-msvc" then
+	config.default_prog = { "powershell", "-nologo" }
+end
+
 -- Appearance
 config.window_padding = { left = 0, right = 0, top = 0, bottom = 0 }
-config.color_scheme = "nightfox"
-config.window_background_opacity = 0.75
+config.color_scheme = "Ashes (base16)"
 
 -- Font
 config.font_size = 12
 config.font = wezterm.font_with_fallback({
-	{ family = "monospace", weight = 500, harfbuzz_features = { "ss01" } },
+	{ family = "Rec Mono Casual", weight = 400, harfbuzz_features = {} },
+	{ family = "Symbols Nerd Font", scale = 1 },
 })
--- config.line_height = 1.1
+-- config.line_height = 1.2
 
 -- Behaviour
 config.enable_tab_bar = false
 config.enable_scroll_bar = false
 config.enable_kitty_graphics = true
+config.enable_wayland = false
 
 return config
