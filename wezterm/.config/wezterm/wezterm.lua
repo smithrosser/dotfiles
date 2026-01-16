d37b8d3804aa150e35dc5d70522dd44ede35b298
Tom Smithrosser <tsmithrosser@roku.com>
2026-01-16 10:50:05 +0000

    update shell/wezterm configs

--- wezterm/.config/wezterm/wezterm.lua
+++ wezterm/.config/wezterm/wezterm.lua
@@ -1,23 +1,47 @@
 local wezterm = require 'wezterm'
-local font_name = 'FiraCode Nerd Font'
+local font_name = 'IntoneMono Nerd Font'
+
+local function get_appearance()
+  if wezterm.gui then
+    return wezterm.gui.get_appearance()
+  end
+  return 'Dark'
+end
+
+local function get_scheme_for_appearance(appearance)
+  if appearance:find 'Dark' then
+    return 'Tokyo Night Moon'
+  else
+    return 'Tokyo Night Day'
+  end
+end
 
 return {
-  color_scheme = 'Tokyo Night Storm',
+  color_scheme = get_scheme_for_appearance(get_appearance()),
   font = wezterm.font {
     family = font_name,
     weight = 500,
   },
   font_rules = {
-    -- disable italics
-    { intensity = 'Normal', italic = true, font = wezterm.font { family = font_name, italic = false, weight = 500 } },
-    { intensity = 'Bold', italic = true, font = wezterm.font { family = font_name, italic = false, weight = 900 } },
+    -- Optionally disable italics
+    -- {
+    --   intensity = 'Normal',
+    --   italic = true,
+    --   font = wezterm.font { family = font_name, italic = false, weight = 500 },
+    -- },
+    -- {
+    --   intensity = 'Bold',
+    --   italic = true,
+    --   font = wezterm.font { family = font_name, italic = false, weight = 900 },
+    -- },
   },
-  font_size = 16,
+  font_size = 14,
   cell_width = 0.9,
 
-  window_padding = { left = 0, right = 0, top = 0, bottom = 0 },
-
   front_end = 'WebGpu',
+  window_decorations = 'TITLE | RESIZE | MACOS_USE_BACKGROUND_COLOR_AS_TITLEBAR_COLOR',
+  window_padding = { left = 20, right = 20, top = 20, bottom = 20 },
+
   enable_wayland = false,
   enable_tab_bar = false,
   enable_scroll_bar = false,
