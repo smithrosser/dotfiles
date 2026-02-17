978223abf4df7b4c319cf905cd779cbb7fe7c595
Tom Smithrosser <tsmithrosser@roku.com>
2026-02-17 09:24:58 +0000

    add extra work utility functions

--- zsh/.config/zsh/work.sh
+++ zsh/.config/zsh/work.sh
@@ -16,14 +16,17 @@ mtrap() {
 # Roku helpers
 rws() {
     case "$1" in
-        "grandcentral" | "gc")
+        "gc")
             cd ~/ws/gc/worktrees/dev/library
             ;;
-        "firmware" | "fw")
+        "fw")
             cd ~/ws/firmware/worktrees/main
             ;;
+        "rpay" )
+            cd ~/ws/roku-pay-lib
+            ;;
         *)
-            echo "Usage: rws [grandcentral | gc, firmware | fw]"
+            echo "Usage: rws [gc, fw, rpay]"
             ;;
     esac
 }
@@ -52,6 +55,32 @@ ccr() {
     ~/ws/stream/ccr.sh
 }
 
+# GC Toolbox 'get services payload' wrapper
+srp() {
+    local basedir="/home/$USER/srp"
+    case "$1" in
+        "clear")
+            rm -rf $basedir/*
+            ;;
+        "get")
+            [[ $# -ne 3 ]] && { echo "usage: srp get [target] [query]"; return 1 }
+
+            local target=$2
+            local query=$3
+            local outdir="$basedir/$(date +'%Y-%m-%d_%H-%M-%S')_T$target-Q$query"
+
+            mkdir -p $outdir
+            get_sr_payload $ROKU_DEV_TARGET --target $target $query -O $outdir || { rm -rf $outdir; return 1 }
+            prettier -w $outdir/*.json # format outputted JSON for readability
+            ;;
+        *)
+            echo "usage: srp {get, clear]"
+            return 1
+            ;;
+    esac
+
+}
+
 # Enable completions (some Roku setup required it)
 autoload -Uz compinit
 zstyle ':completion:*' menu select
@@ -60,4 +89,13 @@ fpath+=~/.zfunc
 # Enable direnv
 if [[ "$COMPUTER_NAME" == "mousetrap" ]]; then
     eval "$(direnv hook zsh)"
+
+    # pyenv python version management
+    export PYENV_ROOT="$HOME/.pyenv"
+    [[ -d $PYENV_ROOT/bin ]] && export PATH="$PYENV_ROOT/bin:$PATH"
+    eval "$(pyenv init - bash)"
+    eval "$(pyenv virtualenv-init -)"
+
+    # Add GC Toolbox to PATH
+    export PATH="$PATH:/home/$USER/ws/gc-toolbox/.venv/bin"
 fi
